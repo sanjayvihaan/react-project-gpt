aeb2f1b5ed1aeb47090e9814bc291dcd6c96507d
V Sanjay <sanjayvihaan111@gmail.com>
2021-12-27 22:59:18 +0530

    creating on the way

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,10 +1,25 @@
 import react from "react";
 import './brand.css'
+import { google, slack, atlassian, dropbox, shopify } from './import'
 
 function Brand() {
     return (
-        <div>
-            
+        <div className="gpt3__brand section__padding">
+            <div>
+                <img src={google} alt="" />
+            </div>
+            <div>
+                <img src={slack} alt="" />
+            </div>
+            <div>
+                <img src={atlassian} alt="" />
+            </div>
+            <div>
+                <img src={dropbox} alt="" />
+            </div>
+            <div>
+                <img src={shopify} alt="" />
+            </div>
         </div>
     )
 }
